a062eb203e8c8ae2e29a3f750ba27016da073ab7
T0mat1 <thomas.rossipro@gmail.com>
2018-04-03 11:14:44 +0200

    Update Matrice

--- Projet_Matrice/Projet_Matrice/CMatrice.h
+++ Projet_Matrice/Projet_Matrice/CMatrice.h
@@ -7,8 +7,38 @@ private:
 	unsigned int uiNombreColonnes;
 
 public:
-	CMatrice()
+	//Constructeurs et destructeur
 
+	CMatrice();
+	/**********************************************
+	*** Constructeur par défaut                 ***
+	***********************************************
+	*** E : néant                               ***
+	*** nécessite : néant                       ***
+	*** S : néant                               ***
+	*** entraine : Matrice 1x1 initialisée à 1  ***
+	**********************************************/
+
+	CMatrice(CMatrice & MATParam);
+	
+	CMatrice(unsigned int uiNombreLignes, unsigned int uiNombreColonnes);
 	~CMatrice();
+
+	//Accesseurs
+	unsigned int MATLireNombreLignes();
+	unsigned int MATLireNombreColonnes();
+
+	void MATModifierComposante(MType tParam, unsigned int uiLigne, unsigned int uiColonne);
+
+	//Surcharges d'operateurs
+	CMatrice operator*(int iParam);
+	CMatrice operator*(CMatrice MATParam);
+	CMatrice operator+(CMatrice MATParam);
+	CMatrice operator-(CMatrice MATParam);
+	CMatrice operator/(CMatrice MATParam);
+
+	//Methodes
+	void MATAfficherMatrice();
+	CMatrice MATCalculerTransposee();
 };
 
