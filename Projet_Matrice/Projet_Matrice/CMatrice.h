af2cabcc0dd0a47de3717a68fdabda348d411bd4
T0mat1 <thomas.rossipro@gmail.com>
2018-04-03 12:07:33 +0200

    Add some comments ;)

--- Projet_Matrice/Projet_Matrice/CMatrice.h
+++ Projet_Matrice/Projet_Matrice/CMatrice.h
@@ -7,7 +7,7 @@ private:
 	unsigned int uiNombreColonnes;
 
 public:
-	//Constructeurs et destructeur
+	// ----------------------------------------------- Constructeurs et destructeur ----------------------------------------------- 
 
 	CMatrice();
 	/**********************************************
@@ -20,24 +20,81 @@ public:
 	**********************************************/
 
 	CMatrice(CMatrice & MATParam);
+	/*************************************************************
+	*** Constructeur de recopie                                ***
+	**************************************************************
+	*** E : MATParam ; la matrice à recopier					   ***
+	*** nécessite : néant									   ***
+	*** S : néant											   ***
+	*** entraine : Matrice initialisée par recopie de MATParam ***
+	*************************************************************/
 	
 	CMatrice(unsigned int uiNombreLignes, unsigned int uiNombreColonnes);
+	/**************************************************************************
+	*** Constructeur par défaut												***
+	***************************************************************************
+	*** E : uiNombreLignes, uiNombreColonnes ; les dimensions de la matrice ***
+	*** nécessite : néant													***
+	*** S : néant															***
+	*** entraine : Matrice 1x1 initialisée à 1								***
+	**************************************************************************/
+
 	~CMatrice();
+	/**************************************************************************
+	*** Destructeur         												***
+	***************************************************************************
+	*** E : néant															***
+	*** nécessite : néant													***
+	*** S : néant															***
+	*** entraine : Destruction de l'objet CMatrice							***
+	**************************************************************************/
 
-	//Accesseurs
-	unsigned int MATLireNombreLignes();
-	unsigned int MATLireNombreColonnes();
 
-	void MATModifierComposante(MType tParam, unsigned int uiLigne, unsigned int uiColonne);
+	//----------------------------------------------- Accesseurs ----------------------------------------------- 
 
-	//Surcharges d'operateurs
+	inline unsigned int MATLireNombreLignes();
+	/************************************************************************************
+	*** Accesseur en lecture   														  ***
+	*************************************************************************************
+	*** E : néant																	  ***
+	*** nécessite : néant															  ***
+	*** S : Nombre de lignes de la matrice (attribut uiNombreLignes) ; entier naturel ***
+	*** entraine : retour de la valeur de uiNombreLignes							  ***
+	************************************************************************************/
+
+	inline unsigned int MATLireNombreColonnes();
+	/****************************************************************************************
+	*** Accesseur en lecture   															  ***
+	*****************************************************************************************
+	*** E : néant																		  ***
+	*** nécessite : néant																  ***
+	*** S : Nombre de lignes de la matrice (attribut uiNombreColonnes) ; entier naturel	  ***
+	*** entraine : retour de la valeur de uiNombreColonnes    							  ***
+	****************************************************************************************/
+
+	inline void MATModifierComposante(MType tParam, unsigned int uiLigne, unsigned int uiColonne);
+	/*************************************************************************************************
+	*** Accesseur en ecriture 																	   ***
+	**************************************************************************************************
+	*** E : - tParam un parametre de type MType qui contient la nouvelle valeur de la composante   ***
+	***		- uiLigne la coordonnee de la ligne de la composante à modifier ; entier naturel	   ***
+	***		- uiColonne la coordonnee de la colonne de la composante à modifier ; entier naturel   ***
+	*** nécessite : néant																		   ***
+	*** S : néant																				   ***
+	*** entraine : modification d'une composante de la matrice aux coordonnées (uiColonne,uiLigne) ***
+	*************************************************************************************************/
+
+
+	//----------------------------------------------- Surcharges d'operateurs ----------------------------------------------- 
+
+
 	CMatrice operator*(int iParam);
 	CMatrice operator*(CMatrice MATParam);
 	CMatrice operator+(CMatrice MATParam);
 	CMatrice operator-(CMatrice MATParam);
-	CMatrice operator/(CMatrice MATParam);
+	CMatrice operator/(int iParam);
 
-	//Methodes
+	//----------------------------------------------- Methodes ----------------------------------------------- 
 	void MATAfficherMatrice();
 	CMatrice MATCalculerTransposee();
 };
