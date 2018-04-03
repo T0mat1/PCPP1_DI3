f40460a89f18f72024b81dfa16b8cde9e61dc9bf
Btrd29 <btrd29@hotmail.fr>
2018-04-03 12:06:02 +0200

    ajout CParseur

---
+++ Projet_Matrice/Projet_Matrice/Parseur.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "CMatrice.h"
+#include <iostream>
+#include <string>
+
+class CParseur
+{
+public:
+	//------------------------------------------------ Constructeurs et destructeur ------------------------------------------------
+	
+	CParseur(void);
+	/**********************************************
+	*** Constructeur par défaut                 ***
+	***********************************************
+	*** E : néant                               ***
+	*** nécessite : néant                       ***
+	*** S : néant                               ***
+	*** entraine : Parseur initialisé           ***
+	**********************************************/
+	~CParseur(void);
+	/*************************************************
+	*** Destructeur par défaut                     ***
+	**************************************************
+	*** E : néant                                  ***
+	*** nécessite : néant                          ***
+	*** S : néant                                  ***
+	*** entraine : Destruction de l'objet CParseur ***
+	*************************************************/
+	
+	//------------------------------------------------ Méthodes ------------------------------------------------
+	CMatrice<double> PARLireFichier(std::string sNomFichier);
+	/**********************************************
+	*** Interpreteur de fichier                 ***
+	***********************************************
+	*** E : le nom du fichier à interpréter     ***
+	*** nécessite : néant                       ***
+	*** S : Un objet CMatrice de type double    ***
+	*** entraine : Creation d'un objet CMatrice ***
+	**********************************************/
+};
+
