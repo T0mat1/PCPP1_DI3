9f9378b9467834af2a1c3ab1b51b94bfce0ee2bd
T0mat1 <thomas.rossipro@gmail.com>
2018-04-06 11:01:20 +0200

    update CMatrice.h (mostly comments)

--- Projet_Matrice/Projet_Matrice/CMatrice.h
+++ Projet_Matrice/Projet_Matrice/CMatrice.h
@@ -12,75 +12,75 @@ public:
 	// ----------------------------------------------- Constructeurs et destructeur ----------------------------------------------- 
 
 	CMatrice();
-	/**********************************************
-	*** Constructeur par défaut                 ***
-	***********************************************
-	*** E : néant                               ***
-	*** nécessite : néant                       ***
-	*** S : néant                               ***
-	*** entraine : Matrice 1x1 initialisée à 1  ***
-	**********************************************/
+	/*************************************************************************************************
+	*** Constructeur par défaut																	   ***
+	**************************************************************************************************
+	*** E : néant																				   ***
+	*** nécessite : néant																		   ***
+	*** S : néant																				   ***
+	*** entraine : Matrice 1x1 initialisée à 1													   ***
+	*************************************************************************************************/
 
 	CMatrice(CMatrice & MATParam);
-	/*************************************************************
-	*** Constructeur de recopie                                ***
-	**************************************************************
-	*** E : MATParam ; la matrice à recopier                   ***
-	*** nécessite : néant									   ***
-	*** S : néant											   ***
-	*** entraine : Matrice initialisée par recopie de MATParam ***
-	*************************************************************/
+	/*************************************************************************************************
+	*** Constructeur de recopie																	   ***
+	**************************************************************************************************
+	*** E : MATParam ; la matrice à recopier													   ***
+	*** nécessite : néant																		   ***
+	*** S : néant																				   ***
+	*** entraine : Matrice initialisée par recopie de MATParam									   ***
+	*************************************************************************************************/
 	
 	CMatrice(unsigned int uiNombreLignes, unsigned int uiNombreColonnes);
-	/**************************************************************************
-	*** Constructeur par défaut												***
-	***************************************************************************
-	*** E : uiNombreLignes, uiNombreColonnes ; les dimensions de la matrice ***
-	*** nécessite : néant													***
-	*** S : néant															***
-	*** entraine : Matrice 1x1 initialisée à 1								***
-	**************************************************************************/
+	/************************************************************************************************
+	*** Constructeur par défaut																	  ***
+	*************************************************************************************************
+	*** E : uiNombreLignes, uiNombreColonnes ; les dimensions de la matrice						  ***
+	*** nécessite : néant																		  ***
+	*** S : néant																				  ***
+	*** entraine : Matrice 1x1 initialisée à 1													  ***
+	************************************************************************************************/
 
 	~CMatrice();
-	/**************************************************************************
-	*** Destructeur         												***
-	***************************************************************************
-	*** E : néant															***
-	*** nécessite : néant													***
-	*** S : néant															***
-	*** entraine : Destruction de l'objet CMatrice							***
-	**************************************************************************/
+	/************************************************************************************************
+	*** Destructeur         																	  ***
+	*************************************************************************************************
+	*** E : néant																				  ***
+	*** nécessite : néant																		  ***
+	*** S : néant																				  ***
+	*** entraine : Destruction de l'objet CMatrice												  ***
+	************************************************************************************************/
 
 
 	//----------------------------------------------- Accesseurs ----------------------------------------------- 
 
 	inline unsigned int MATLireNombreLignes();
-	/************************************************************************************
-	*** Accesseur en lecture   														  ***
-	*************************************************************************************
-	*** E : néant																	  ***
-	*** nécessite : néant															  ***
-	*** S : Nombre de lignes de la matrice (attribut uiNombreLignes) ; entier naturel ***
-	*** entraine : retour de la valeur de uiNombreLignes							  ***
-	************************************************************************************/
+	/*************************************************************************************************
+	*** Accesseur en lecture   																	   ***
+	**************************************************************************************************
+	*** E : néant																				   ***
+	*** nécessite : néant																		   ***
+	*** S : Nombre de lignes de la matrice (attribut uiNombreLignes) ; entier naturel			   ***
+	*** entraine : retour de la valeur de uiNombreLignes										   ***
+	*************************************************************************************************/
 
 	inline unsigned int MATLireNombreColonnes();
-	/****************************************************************************************
-	*** Accesseur en lecture   															  ***
-	*****************************************************************************************
-	*** E : néant																		  ***
-	*** nécessite : néant																  ***
-	*** S : Nombre de lignes de la matrice (attribut uiNombreColonnes) ; entier naturel	  ***
-	*** entraine : retour de la valeur de uiNombreColonnes    							  ***
-	****************************************************************************************/
+	/*************************************************************************************************
+	*** Accesseur en lecture   																	   ***
+	**************************************************************************************************
+	*** E : néant																				   ***
+	*** nécessite : néant																		   ***
+	*** S : Nombre de lignes de la matrice (attribut uiNombreColonnes) ; entier naturel			   ***
+	*** entraine : retour de la valeur de uiNombreColonnes    									   ***
+	*************************************************************************************************/
 
 	inline void MATModifierComposante(MType tParam, unsigned int uiLigne, unsigned int uiColonne);
 	/*************************************************************************************************
-	*** Accesseur en ecriture 																	   ***
+	*** Accesseur en écriture 																	   ***
 	**************************************************************************************************
-	*** E : - tParam un parametre de type MType qui contient la nouvelle valeur de la composante   ***
-	***		- uiLigne la coordonnee de la ligne de la composante à modifier ; entier naturel	   ***
-	***		- uiColonne la coordonnee de la colonne de la composante à modifier ; entier naturel   ***
+	*** E : - tParam un paramètre de type MType qui contient la nouvelle valeur de la composante   ***
+	***		- uiLigne la coordonnée de la ligne de la composante à modifier ; entier naturel	   ***
+	***		- uiColonne la coordonnée de la colonne de la composante à modifier ; entier naturel   ***
 	*** nécessite : néant																		   ***
 	*** S : néant																				   ***
 	*** entraine : modification d'une composante de la matrice aux coordonnées (uiColonne,uiLigne) ***
@@ -89,16 +89,78 @@ public:
 
 	//----------------------------------------------- Surcharges d'operateurs ----------------------------------------------- 
 
-
 	CMatrice operator*(int iParam);
+	/*************************************************************************************************
+	*** Surcharge de l'opérateur * (multiplication avec un entier)								   ***
+	**************************************************************************************************
+	*** E : iParam : un entier																	   ***
+	*** nécessite : néant																		   ***
+	*** S : Un objet CMatrice, produit d'un CMatrice et d'un entier								   ***
+	*** entraine : néant																		   ***
+	*************************************************************************************************/
+	
 	CMatrice operator*(CMatrice MATParam);
+	/*************************************************************************************************
+	*** Surcharge de l'opérateur * (multiplication avec une matrice)							   ***
+	**************************************************************************************************
+	*** E : MATParam : un objet CMatrice														   ***
+	*** nécessite : néant																		   ***
+	*** S : Un objet CMatrice, produit matriciel de deux objets CMatrice						   ***
+	*** entraine : néant																		   ***
+	*************************************************************************************************/
+
 	CMatrice operator+(CMatrice MATParam);
+	/*************************************************************************************************
+	*** Surcharge de l'opérateur + (addition avec une matrice)									   ***
+	**************************************************************************************************
+	*** E : MATParam : un objet CMatrice														   ***
+	*** nécessite : néant																		   ***
+	*** S : Un objet CMatrice, somme de deux objets CMatrice									   ***
+	*** entraine : néant																		   ***
+	*************************************************************************************************/
+
 	CMatrice operator-(CMatrice MATParam);
+	/*************************************************************************************************
+	*** Surcharge de l'opérateur - (soustraction par une matrice)								   ***
+	**************************************************************************************************
+	*** E : MATParam : un objet CMatrice														   ***
+	*** nécessite : néant																		   ***
+	*** S : Un objet CMatrice, différence entre la matrice courante et MATParam					   ***
+	*** entraine : néant																		   ***
+	*************************************************************************************************/
+	
 	CMatrice operator/(int iParam);
+	/*************************************************************************************************
+	*** Surcharge de l'opérateur / (division par un entier)										   ***
+	**************************************************************************************************
+	*** E : MATParam : un objet CMatrice														   ***
+	*** nécessite : néant																		   ***
+	*** S : Un objet CMatrice, où chaque composante vaut										   ***
+	***		celle de la matrice courante divisée par iParam								           ***
+	*** entraine : néant																		   ***
+	*************************************************************************************************/
 
 	//----------------------------------------------- Methodes ----------------------------------------------- 
+	
 	void MATAfficherMatrice();
+	/*************************************************************************************************
+	*** Méthode d'affichage d'une matrice dans la console										   ***
+	**************************************************************************************************
+	*** E : néant																				   ***
+	*** nécessite : néant																		   ***
+	*** S : néant																				   ***
+	*** entraine : Affichage de la matrice dans la console										   ***
+	*************************************************************************************************/
+	
 	CMatrice MATCalculerTransposee();
+	/*************************************************************************************************
+	*** Méthode de calcul de la transposée d'une matrice										   ***
+	**************************************************************************************************
+	*** E : néant																				   ***
+	*** nécessite : néant																		   ***
+	*** S : Un objet CMatrice égal à la transposée de la matrice								   ***
+	*** entraine :																				   ***
+	*************************************************************************************************/
 };
 
 #endif
