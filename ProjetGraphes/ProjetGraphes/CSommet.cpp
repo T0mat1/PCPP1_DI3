149547823219429bcb681a8a75bc1ea24ba253ba
Btrd29 <btrd29@hotmail.fr>
2018-04-25 12:16:35 +0200

    CSommet updated and should work fine now

--- ProjetGraphes/ProjetGraphes/CSommet.cpp
+++ ProjetGraphes/ProjetGraphes/CSommet.cpp
@@ -1,11 +1,42 @@
 #include "CSommet.h"
+#include <algorithm>
+using namespace std;
 
 
 CSommet::CSommet(void)
 {
 }
 
+CSommet::CSommet(unsigned int uiNumero)
+{
+	uiSOMNumero = uiNumero;
+}
 
 CSommet::~CSommet(void)
 {
+}
+
+void CSommet::SOMAjouterArcArrivant(CArc ARCParam)
+{
+	vSOMArrivants.push_back(ARCParam);
+}
+
+void CSommet::SOMAjouterArcPartant(CArc ARCParam)
+{
+	vSOMPartants.push_back(ARCParam);
+}
+
+void CSommet::SOMSupprimerArcArrivant(CArc ARCParam)
+{
+	vSOMArrivants.erase(remove(vSOMArrivants.begin(), vSOMArrivants.end(), ARCParam), vSOMArrivants.end());
+}
+
+void CSommet::SOMSupprimerArcPartant(CArc ARCParam)
+{
+	vSOMPartants.erase(remove(vSOMPartants.begin(), vSOMPartants.end(), ARCParam), vSOMPartants.end());
+}
+
+unsigned int CSommet::SOMLireNumero()
+{
+	return uiSOMNumero;
 }
